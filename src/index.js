dc8dfaba4ee29cc16a6a094b8a16af725f482d4c
V A Magendran <95860580+Magendran001@users.noreply.github.com>
2022-03-02 18:22:50 +0530

    module

---
+++ src/index.js
@@ -0,0 +1,10 @@
+const addition = require("./add");
+const multiplication = require("./multipy");
+
+const subtract = require("./subtract");
+const divide = require("./divide");
+
+console.log(addition(5, 4));
+console.log(multiplication(5, 4));
+console.log(subtract(5, 4));
+console.log(divide(5, 4));
---
+++ src/multipy.js
@@ -0,0 +1,3 @@
+module.exports = (a, b) => {
+  return { multiplication: a * b };
+};
